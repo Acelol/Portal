12e6a8b80cc251b32d421f792128d02553ad8b56
Acelol <40193816+Acelol@users.noreply.github.com>
2020-04-09 00:49:43 +0800

    修改注释

--- js/newsdatainput.js
+++ js/newsdatainput.js
@@ -9,19 +9,10 @@ $(document).ready(function(){
 	       $.each(data.result, function(i, item) {
 			   var title='<div class="hot_news_item"><div class="hot_news_item_content"><div class="hot_news_item_title">'+item.title+'</div><div class="hot_news_item_author">'+item.author+'</div><div class="hot_news_item_time">2020-'+item.time+'发布</div><div class="hot_news_item_status">'+item.status_DISPLAY+'</div></div><div class="hot_news_item_pic"><img src='+item.img+' /></div></div>';
 	            document.getElementById("hot_news_list").innerHTML= document.getElementById("hot_news_list").innerHTML+title;
-	       })
+	       });
 	   },
 	   error:function(){
 			console.log(1);
 	   }
 	})
-// 	$('.hot_news_item').each(function(){
-// 		$(this).click(function(){
-// 			alert(1);
-// 		});
-// 	});
-// 	$('hot_news_item').on('click',function(){
-// 		alert(1);
-// 	});
-
 });
